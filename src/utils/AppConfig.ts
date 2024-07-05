ddf5962a2f4885bd5c001a5286a77e13d200d335
Reality361 <154047191+Reality361@users.noreply.github.com>
2024-07-05 20:48:01 +0800

    Delete src directory

--- src/utils/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-export const AppConfig = {
-  site_name: 'Reality361 Blog',
-  title: 'Reality361 Blog',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Reality361',
-  locale_region: 'en-us',
-  locale: 'en',
-};
